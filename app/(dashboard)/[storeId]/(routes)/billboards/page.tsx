3d34588d3ab45532396471ce80cbfae8becb34da
JoaquinGuzman <149431379+Joaco1404@users.noreply.github.com>
2024-05-28 02:34:38 -0300

    Modificación e importaciónpage.tsx

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,4 +1,7 @@
+import { format } from "date-fns";
+
 import { BillboardClient } from "./components/client";
+import { BillboardColumn } from "./components/columns";
 
 import prismadb from "@/lib/prismadb";
 
@@ -15,11 +18,17 @@ const BillboardsPage = async ({
       createdAt: 'desc'
     }
   });
+
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, "MMMM do, yyyy")
+  }));
   
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={billboards} />
+        <BillboardClient data={formattedBillboards} />
       </div>
     </div>
   );
