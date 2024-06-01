b2ef7c1ecfbbef4bcba7c2539629cedb1c94fc11
BugaBuga <137830634+fergj22@users.noreply.github.com>
2024-06-01 08:54:17 -0300

    Color Entity

---
+++ app/api/[storeId]/colors/colorId/route.ts
@@ -0,0 +1,122 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { colorId: string } }
+) {
+  try {
+    if (!params.colorId) {
+      return new NextResponse("Id del Color es necesario", { status: 400 });
+    }
+
+    const color = await prismadb.color.findUnique({
+      where: {
+        id: params.colorId,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLOR_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; colorId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name, value } = body;
+
+    if (!userId) {
+      return new NextResponse("No Autenticado", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Nombre es requerido", { status: 400 });
+    }
+
+    if (!value) {
+      return new NextResponse("EL valor es requerido", { status: 400 });
+    }
+
+    if (!params.colorId) {
+      return new NextResponse("El id del Color es necesario", {
+        status: 400,
+      });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    //esto hace que no se pueda cambiar las tiendas de otros usuarios
+    if (!storeByUserId) {
+      return new NextResponse("Sin autorización", { status: 403 });
+    }
+
+    const color = await prismadb.color.updateMany({
+      where: {
+        id: params.colorId,
+      },
+      data: {
+        name,
+        value,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLOR_PATCH]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeId: string; colorId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("No Autenticado", { status: 401 });
+    }
+
+    if (!params.colorId) {
+      return new NextResponse("Id del Color necesario", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    //esto hace que no se pueda cambiar las tiendas de otros usuarios
+    if (!storeByUserId) {
+      return new NextResponse("Sin autorización", { status: 403 });
+    }
+
+    const color = await prismadb.color.deleteMany({
+      where: {
+        id: params.colorId,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("COLOR_DELETE]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
