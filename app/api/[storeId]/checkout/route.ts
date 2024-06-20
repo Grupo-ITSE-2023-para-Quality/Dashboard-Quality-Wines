b1b155dae9ff1fbf3b854fec0e2bdae0116768a6
ProfeCeci <andreacecilopez88@gmail.com>
2024-06-20 14:07:31 -0300

    checkout

---
+++ app/api/[storeId]/checkout/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server";
+import prismadb from "@/lib/prismadb";
+import { connect } from "http2";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+    const { productIds } = await req.json();
+
+    if (!productIds || productIds.length === 0) {
+        return new NextResponse("Ids de productos son obligatorios", { status: 400 });
+    }
+
+    const products = await prismadb.product.findMany({
+        where: {
+            id: {
+                in: productIds
+            }
+        }
+    });
+
+    
+    const order = await prismadb.order.create({
+        data:{
+            storeId: params.storeId,
+            isPaid: false,
+            orderItems: {
+                create: productIds.map((productId: string) => ({
+                    product: {
+                        connect: {
+                            id: productId
+                        }
+                    }
+                }))
+            }
+        }
+    });
+    
+  
+}
